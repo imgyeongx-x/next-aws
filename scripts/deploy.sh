cd6aa6e75bc9bc406d50d834b220031d1027c22b
choing <imgyeong00@gmail.com>
2026-04-15 04:15:50 +0900

    Fix: SSH keepalive

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,14 +4,24 @@ set -Eeuo pipefail
 APP_DIR="/var/www/next-aws"
 NVM_DIR="$HOME/.nvm"
 
+echo "[1/6] load nvm"
 export NVM_DIR
 [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
 
+echo "[2/6] move to app dir"
 cd "$APP_DIR"
 
+echo "[3/6] versions"
+node -v
+npm -v
+
+echo "[4/6] npm ci"
 npm ci
+
+echo "[5/6] build"
 npm run build
 
+echo "[6/6] restart pm2"
 if pm2 describe next-aws >/dev/null 2>&1; then
   pm2 restart next-aws
 else
@@ -19,3 +29,4 @@ else
 fi
 
 pm2 save
+echo "[done] deploy finished"
